7680374a9fcb05cbf8e4a3ba3ae1097a2fc911cc
Christian Georgi <christian.georgi@sap.com>
2020-07-31 12:36:11 +0200

    Do no longer use run blocks - removed in cds 4

--- orders/srv/orders-service.js
+++ orders/srv/orders-service.js
@@ -5,17 +5,19 @@ module.exports = cds.service.impl(function() {
   const { Books } = cds.entities
 
   // Reduce stock of ordered books if available stock suffices
-  this.before ('CREATE', 'Orders', (req) => {
+  this.before ('CREATE', 'Orders', async (req) => {
     const { Items: OrderItems } = req.data
-    return cds.transaction(req) .run (()=> OrderItems.map (order =>
+    const tx = cds.transaction(req)
+    const result = await Promise.all (OrderItems.map (order => tx.run (
       UPDATE (Books) .where ('ID =', order.book_ID)
       .and ('stock >=', order.amount)
       .set ('stock -=', order.amount)
-    )) .then (all => all.forEach ((affectedRows,i) => {
+    )) )
+    result.forEach ((affectedRows,i) => {
       if (affectedRows === 0)  req.error (409,
         `${OrderItems[i].amount} exceeds stock for book #${OrderItems[i].book_ID}`
       )
-    }))
+    })
   })
 
 })
